bdf29ccd523158de62046b0a6636c0fdbcf26e8d
kezi <me@kez.io>
2019-04-08 04:52:06 -0400

    Initial Commit

---
+++ KZ9046/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KZ9046
+//
+//  Created by kezi on 4/8/31 H.
+//  Copyright © 31 Heisei kezi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KZ9046Tests/KZ9046Tests.swift
@@ -0,0 +1,34 @@
+//
+//  KZ9046Tests.swift
+//  KZ9046Tests
+//
+//  Created by kezi on 4/8/31 H.
+//  Copyright © 31 Heisei kezi. All rights reserved.
+//
+
+import XCTest
+@testable import KZ9046
+
+class KZ9046Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
